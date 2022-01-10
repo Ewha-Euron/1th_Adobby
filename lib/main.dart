694aac1f12bab5d11710247f13c18eb034162452
Mijin Koo <79077316+mijinkoo@users.noreply.github.com>
2022-01-10 21:01:28 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:get/get.dart';
-import 'datePick.dart';
-import 'diaryList.dart';
-import 'mainPage.dart';
-
-// import 'diaryContent.dart';;
-// import 'inferenceResult.dart';
-// import 'calendar.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      getPages: [
-        GetPage(name: "/", page: () => MainPage()),
-        GetPage(name: '/diarylist', page: () => DiaryList()),
-        GetPage(name: '/datepick', page: () => DatePick()),
-      ],
-    );
-  }
-}
-=======
 import 'package:adobby/screens/MainScreen.dart';
 
 void main() => runApp(MyApp());
@@ -43,4 +14,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
->>>>>>> afdfb24fdf58ccbe0d04f9a906c961d2b852c050
