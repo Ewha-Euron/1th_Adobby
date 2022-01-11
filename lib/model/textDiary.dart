ee945ef2759f4b10f9763331e6cbc928a5e4820c
KJI-DEC <kji_dec22@ewhain.net>
2022-01-11 22:40:56 +0900

    Add: textDiary 추가

---
+++ lib/model/textDiary.dart
@@ -0,0 +1,12 @@
+class TextDiary {
+  String text;
+  int date;
+  String androidId;
+
+  TextDiary({required this.text, required this.date, required this.androidId});
+
+  factory TextDiary.fromJson(Map<String, dynamic> json) {
+    return TextDiary(
+        text: json['text'], date: json['date'], androidId: json['androidId']);
+  }
+}
